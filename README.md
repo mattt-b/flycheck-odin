1c0570a2df2417f733624df7add46b756e2e198d
matt <matt@matttb.com>
2019-01-06 23:59:17 -0700

    Initial implementation

---
+++ README.md
@@ -0,0 +1,85 @@
+# flycheck-odin
+[Flycheck](https://www.flycheck.org/en/latest/) for [Odin](https://github.com/odin-lang/Odin)
+
+## Setup
+```elisp
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-odin-setup))
+```
+
+## Contents
+* [Default behavior](#default-behavior)
+* [Change project checker root](#change-project-checker-root)
+* [Filter errors](#filter-errors)
+* [Examples](#examples)
+
+#### Default behavior
+This is a wrapper around `odin check <location> -vet`. With no changes on the users' part, the default
+behavior is is to run `odin check . -vet` (where '.' is the current buffer directory).
+
+
+#### Change project checker root
+There are two methods of setting the root for `odin check`.
+1. Create a `.odin-check` file in the folder that should be treated as root. Any buffer that is being edited will search
+upwards through parent directories looking for a `.odin-check` file. If it finds this file it will insert that directory
+as the `dir` in `odin check dir -vet`. This is useful for any nested project.
+2. Create a `.odin-check` file in any parent folder of the files being edited. The file may include a 
+`path <path/to/make/root>` line. The first instance of the line will be parsed as the `dir` in `odin check dir -vet`. Paths 
+starting with `~` or `/` will be treated as absolute. All others will be treated as relative to the `.odin-check` directory. 
+This is useful for projects that may contain multiple roots (say if one wants to keep the main code and tests separate),
+or if one has code inside a child directory and wants to keep configuration files in the version control root. Can also 
+be used to run odin check using a file as root instead of a directory (say if one is testing two different files with 
+a `main` proc).
+
+Be a good citizen and add `.odin-check` to your `.gitignore_global`
+
+
+#### Filter errors
+A `.odin-check` file may include a `filter <emacs_regex>` line. These will be matched against all errors returned- any error 
+that matches will be removed and won't show up in emacs. This is useful for when you may not want to deal with 
+someone elses' `-vet` errors from a shared library or similar.
+
+
+#### Examples
+Practical `.odin-check` file:
+```
+path src
+filter ^[^[:blank:]]*/Odin/core/
+filter ^[^[:blank:]]*/Odin/shared/
+```
+
+Extra explanation:
+```
+# .odin-check
+# Lines beginning with # are ignored
+
+# This would run
+# odin check <directory/of/this/file>/src -vet
+path src/
+
+# This would run
+# odin check /home/users/foo/code/project/src/main.odin
+# if there wasn't already a path line above
+path /home/users/foo/code/project/src/main.odin
+
+# Filter out errors coming from 'shared' libraries
+filter ^[^[:blank:]]*/Odin/shared/
+
+# Filter out some specific error
+filter declared but not used
+
+# An easy way to test your filters is to run odin check <dir> -vet
+# paste the output of that command into a buffer and then run re-builder
+# this will allow you to play around with your regex and show you the matches
+
+
+filter # There are not inline comments. It will try to filter this line
+
+# Leading whitespace and whitespace after keywords is ignored
+    filter         [[:blank:]]foo
+    
+# There are no multilines
+# this will only filter the first line
+filter some_long_regex
+split_between_lines
+```
---
+++ flycheck-odin.el
@@ -0,0 +1,104 @@
+;;; flycheck-odin.el - Flycheck support for Odin
+
+(require 'flycheck)
+;; Flycheck requires seq so this shouldn't be an issue?
+;; Older versions may not require this
+(require 'seq)
+
+
+(defun flycheck-odin-check-path ()
+  (let ((odin-check-dir (locate-dominating-file default-directory ".odin-check"))
+        (check-path "."))
+    (if (not odin-check-dir)
+        ;; No .odin-check file. Run the equivalent of odin check '.'
+        (expand-file-name check-path)
+      ;; There is a .odin-check file
+      ;; Set the check-path to the dir of the .odin-check file
+      ;; If there is no 'path' line in .odin-check this is the result that
+      ;; will be returned. Otherwise the path param will be parsed and returned
+      (setq check-path odin-check-dir)
+      (with-temp-buffer
+        (insert-file-contents (concat odin-check-dir ".odin-check"))
+        (goto-char (point-min))
+        (if (re-search-forward "^[[:blank:]]*path[[:blank:]]+[^\n]" (point-max) t)
+            ;; Matched a 'path' line
+            (progn
+              ;; The regex above overmatches by one char in order to not
+              ;; match 'path \n'. Move back one char to get actual boundary
+              (backward-char)
+              (if (or (equal (following-char) ?/)
+                      (equal (following-char) ?~)
+                      ;; TODO: Windows absolute paths
+                      )
+                  ;; Absolute path
+                  (setq check-path (thing-at-point 'filename 'no-properties))
+                ;; Relative path
+                (setq check-path (concat odin-check-dir (thing-at-point 'filename 'no-properties)))))
+          )
+        (expand-file-name check-path)))))
+
+(defun flycheck-odin-filter-regexes (odin-check-dir)
+  (let ((regexes '()))
+    (with-temp-buffer
+      (insert-file-contents (concat odin-check-dir ".odin-check"))
+      (goto-char (point-min))
+      (while (re-search-forward "^[[:blank:]]*filter[[:blank:]]+[^\n]" (point-max) t)
+        ;; The regex above overmatches by one char in order to not
+        ;; match 'filter \n'. Move back one char to get actual boundary
+        (backward-char)
+        (push (buffer-substring-no-properties (point) (line-end-position))
+              regexes)))
+    regexes))
+
+(defun flycheck-odin-error-to-check-string (err)
+  "Takes a flycheck-error struct and turns it back into
+the format that odin check spits out"
+  (format "%s(%d:%d) %s"
+          (flycheck-error-filename err)
+          (flycheck-error-line err)
+          (flycheck-error-column err)
+          (flycheck-error-message err)))
+
+(defun flycheck-odin-error-is-not-filtered (filters error)
+  (not (seq-some (lambda (filter)
+                   (string-match-p
+                    filter
+                    (flycheck-odin-error-to-check-string error)))
+                 filters)))
+
+(defun flycheck-odin-error-filter (errors)
+  (let ((odin-check-dir (locate-dominating-file default-directory ".odin-check"))
+        (filter-regexes '()))
+    (if (not odin-check-dir)
+        ;; No .odin-check file, don't filter errors
+        errors
+      ;; .odin-check file. See if there are filters to apply and apply them to the errors list
+      (setq filter-regexes (flycheck-odin-filter-regexes odin-check-dir))
+      (if (not filter-regexes)
+          ;; There were no regexes in the .odin-check file
+          errors
+        ;; We have regexes, filter and return the filtered errors
+        (seq-filter
+         (apply-partially 'flycheck-odin-error-is-not-filtered filter-regexes)
+         errors)))))
+
+(flycheck-define-checker odin
+  "Flycheck checker using odin check -vet"
+  :command ("odin"
+            "check"
+            (eval (flycheck-odin-check-path))
+            "-vet")
+  :error-patterns
+  ((error line-start (file-name) "(" line ":" column ") " (message) line-end))
+  :error-filter flycheck-odin-error-filter
+  :modes (odin-mode)
+  :predicate flycheck-buffer-saved-p)
+
+;;;###autoload
+(defun flycheck-odin-setup ()
+  (interactive)
+  (add-to-list 'flycheck-checkers 'odin))
+
+(provide 'flycheck-odin)
+
+;;; flycheck-odin.el ends here
