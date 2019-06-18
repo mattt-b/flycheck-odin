03bff8fd1ba1c433bc57fc264e49ae50eaa05298
matt <matt@matttb.com>
2019-06-18 12:41:15 -0700

    Rework to use defcustom instead of custom config file

--- flycheck-odin.el
+++ flycheck-odin.el
@@ -5,50 +5,23 @@
 ;; Older versions may not require this
 (require 'seq)
 
+(defgroup flycheck-odin nil
+  "Flycheck checker for Odin"
+  :group 'odin)
+
+(defcustom flycheck-odin-project-path nil
+  "Project to run odin check on.
+Will usually be a directory, but can be a single file."
+  :type 'string
+  :group 'flycheck-odin)
+
+(defcustom flycheck-odin-error-filters '()
+  "Regexes for flycheck to ignore when reporting errors"
+  :type '(repeat string)
+  :group 'flycheck-odin)
 
 (defun flycheck-odin-check-path ()
-  (let ((odin-check-dir (locate-dominating-file default-directory ".odin-check"))
-        (check-path buffer-file-name))
-    (if (not odin-check-dir)
-        ;; No .odin-check file. Run odin check buffer-file-name
-        (expand-file-name check-path)
-      ;; There is a .odin-check file
-      ;; Set the check-path to the dir of the .odin-check file
-      ;; If there is no 'path' line in .odin-check this is the result that
-      ;; will be returned. Otherwise the path param will be parsed and returned
-      (setq check-path odin-check-dir)
-      (with-temp-buffer
-        (insert-file-contents (concat odin-check-dir ".odin-check"))
-        (goto-char (point-min))
-        (if (re-search-forward "^[[:blank:]]*path[[:blank:]]+[^\n]" (point-max) t)
-            ;; Matched a 'path' line
-            (progn
-              ;; The regex above overmatches by one char in order to not
-              ;; match 'path \n'. Move back one char to get actual boundary
-              (backward-char)
-              (if (or (equal (following-char) ?/)
-                      (equal (following-char) ?~)
-                      ;; TODO: Windows absolute paths
-                      )
-                  ;; Absolute path
-                  (setq check-path (thing-at-point 'filename 'no-properties))
-                ;; Relative path
-                (setq check-path (concat odin-check-dir (thing-at-point 'filename 'no-properties)))))
-          )
-        (expand-file-name check-path)))))
-
-(defun flycheck-odin-filter-regexes (odin-check-dir)
-  (let ((regexes '()))
-    (with-temp-buffer
-      (insert-file-contents (concat odin-check-dir ".odin-check"))
-      (goto-char (point-min))
-      (while (re-search-forward "^[[:blank:]]*filter[[:blank:]]+[^\n]" (point-max) t)
-        ;; The regex above overmatches by one char in order to not
-        ;; match 'filter \n'. Move back one char to get actual boundary
-        (backward-char)
-        (push (buffer-substring-no-properties (point) (line-end-position))
-              regexes)))
-    regexes))
+  (or flycheck-odin-project-path default-directory))
 
 (defun flycheck-odin-error-to-check-string (err)
   "Takes a flycheck-error struct and turns it back into
@@ -67,20 +40,11 @@ the format that odin check spits out"
                  filters)))
 
 (defun flycheck-odin-error-filter (errors)
-  (let ((odin-check-dir (locate-dominating-file default-directory ".odin-check"))
-        (filter-regexes '()))
-    (if (not odin-check-dir)
-        ;; No .odin-check file, don't filter errors
-        errors
-      ;; .odin-check file. See if there are filters to apply and apply them to the errors list
-      (setq filter-regexes (flycheck-odin-filter-regexes odin-check-dir))
-      (if (not filter-regexes)
-          ;; There were no regexes in the .odin-check file
-          errors
-        ;; We have regexes, filter and return the filtered errors
-        (seq-filter
-         (apply-partially 'flycheck-odin-error-is-not-filtered filter-regexes)
-         errors)))))
+  (if (not flycheck-odin-error-filters)
+      errors
+    (seq-filter
+     (apply-partially 'flycheck-odin-error-is-not-filtered flycheck-odin-error-filters)
+     errors)))
 
 (flycheck-define-checker odin
   "Flycheck checker using odin check -vet"
